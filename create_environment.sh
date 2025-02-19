9c804283e8e9ec8053188a1a404e3530c82fab50
Oluwaloseyitowi Iji <o.iji@alustudent.com>
2025-02-19 22:46:12 +0200

    creating subdirectories

--- create_environment.sh
+++ create_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "What is your name?"
 read NAME
-directoryName = 
-mkdir "submission_reminder_$name"
+directoryName=submission_reminder_$name
+mkdir $directoryName
 mkdir -p $directoryName/{app,modules,assets,config}
